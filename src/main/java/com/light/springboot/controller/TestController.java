ef31195bd48af3e2a8cf72e97da2b7329bd96bed
wangyuling911 <1134344850@qq.com>
2021-06-08 11:22:33 +0800

    kafka

--- src/main/java/com/light/springboot/controller/TestController.java
+++ src/main/java/com/light/springboot/controller/TestController.java
@@ -1,10 +1,6 @@
 package com.light.springboot.controller;
 
-import com.light.springboot.es.document.KnowledgeEntryDocument;
-import com.light.springboot.es.repository.knowledgeEntryDocumentRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,40 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class TestController {
 
-    @Autowired
-    private ElasticsearchTemplate elasticsearchTemplate;
-
-    @Autowired
-    private knowledgeEntryDocumentRepository knowledgeEntryDocumentRepository;
 
     @GetMapping("/helloworld")
     public String helloworld() {
         log.info("beijign ");
-        return null;
-    }
-
-    @GetMapping("/delete")
-    public String delete() {
-        boolean b = elasticsearchTemplate.deleteIndex(KnowledgeEntryDocument.class);
-        return String.valueOf(b);
-    }
-
-    @GetMapping("/createIndex")
-    public String createIndex() {
-        boolean index = elasticsearchTemplate.createIndex(KnowledgeEntryDocument.class);
-
-        return String.valueOf(index);
-    }
-
-    @GetMapping("/putMapping")
-    public String putMapping() {
-        boolean b = elasticsearchTemplate.putMapping(KnowledgeEntryDocument.class);
-        return String.valueOf(b);
-    }
-
-    @GetMapping("/search")
-    public String search() {
-        return String.valueOf(true);
+        return "beijign";
     }
 
 
