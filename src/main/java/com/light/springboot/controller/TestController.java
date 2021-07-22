4f513cceb2d9635e440393389521693a86f4f881
wangyuling911 <1134344850@qq.com>
2021-07-22 11:20:23 +0800

    各种钩子接口

--- src/main/java/com/light/springboot/controller/TestController.java
+++ src/main/java/com/light/springboot/controller/TestController.java
@@ -10,6 +10,10 @@ import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
 @RestController
 @Slf4j
 public class TestController {
@@ -28,12 +32,43 @@ public class TestController {
 
     @EventListener
     public void optionBaseEvent(TestEvent event) {
+
         System.out.println("EventListener");
         var var = "wang";
     }
 
+    public static void main(String[] args) {
+
+        for (int i = 0; i <100 ; i++) {
+            System.out.println("------------"+i);
+            int b = i & 7;
+            System.out.println(b);
+        }
+//        int[] nums = new int[]{1, 5, 4, 2, 3, 6};
+//        System.out.println(topKMax(nums, 5));    //输出：[2, 3, 4, 5, 6]
+
+    }
 
 
+    //寻找前k个最大的数--使用小顶堆
+    public static List<Integer> topKMax(int[] nums, int k) {
+        //寻找前k个最小数，因此将小顶堆大小定义为k
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k);
+        for (int i = 0; i < nums.length; i++) {
+            if (i < k) {
+                pq.offer(nums[i]);    //前k个数，直接入堆
+            } else if (nums[i] > pq.peek()) {    //如果当前元素比堆顶元素大
+                pq.poll();    //说明堆顶元素（堆中最小元素）一定不属于前k大的数，出堆
+                pq.offer(nums[i]);    //当前元素有可能属于前k大，入堆
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            ans.add(pq.poll());
+        }
+        return ans;
+    }
 
 
 }
