7fdd70fd211a195d2ae7a36b3491a92c11e28ac5
wangyuling911 <1134344850@qq.com>
2021-09-08 11:47:36 +0800

    网页

--- src/main/java/com/light/springboot/controller/TestController.java
+++ src/main/java/com/light/springboot/controller/TestController.java
@@ -4,16 +4,23 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 @Slf4j
 public class TestController {
 
 
+
+
     @GetMapping("/helloworld")
     public String helloworld() {
-        log.info("beijign ");
         return "beijign";
     }
 
+    public static void main(String[] args) {
+        HashMap hashMap = new HashMap(12);
+        hashMap.put("fdsaf","f");
+    }
 
 }
--- src/main/java/com/light/springboot/controller/ThymeleafController.java
+++ src/main/java/com/light/springboot/controller/ThymeleafController.java
@@ -4,10 +4,12 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
@@ -20,8 +22,14 @@ import java.util.UUID;
 public class ThymeleafController {
 
     @RequestMapping("hello")
-    public String hello(Map<String,Object> map) {
-        map.put("msg", "Hello Thymeleaf");
+    public String hello(HttpServletRequest request, HttpServletResponse response) {
+
+        Cookie cookie = new Cookie("wag","yu");
+        //cookie.setSecure(true);
+        // 设置path到根目录
+        cookie.setPath("/");
+        response.addCookie(cookie);
+//        map.put("msg", "Hello Thymeleaf");
         return "hello";
     }
     @RequestMapping("Upfild")
