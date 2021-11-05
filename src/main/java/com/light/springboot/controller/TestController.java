cb3665442cc28a6be0e81d451375573268710842
wangyuling911 <1134344850@qq.com>
2021-11-05 17:43:57 +0800

    linux

--- src/main/java/com/light/springboot/controller/TestController.java
+++ src/main/java/com/light/springboot/controller/TestController.java
@@ -5,11 +5,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.List;
+
 @RestController
 @Slf4j
 public class TestController {
 
     MyThread myThread;
+
     @GetMapping("/helloworld")
     public String helloworld(int s) {
         myThread = new MyThread();
@@ -22,6 +27,18 @@ public class TestController {
     public String stop() {
         myThread.stopMy();
         return "beijign";
+    }
+
+    public static void main21(String[] args) {
+
+        List<GarbageCollectorMXBean> garbageCollectorMXBeans = ManagementFactory.getGarbageCollectorMXBeans();
+        garbageCollectorMXBeans.forEach(a -> {
+            System.out.println(a.getName());
+        });
+    }
+
+    public static void main(String[] args) {
+
     }
 
 }
