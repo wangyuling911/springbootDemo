f133957cd4d44960d0a41861eac7c70fcab1d133
wangyuling911 <1134344850@qq.com>
2021-11-23 18:34:19 +0800

    redis

--- src/main/java/com/light/springboot/controller/TestController.java
+++ src/main/java/com/light/springboot/controller/TestController.java
@@ -1,8 +1,8 @@
 package com.light.springboot.controller;
 
 
-import com.light.springboot.vo.User;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.RandomStringUtils;
 import org.apache.tomcat.util.http.SameSiteCookies;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
@@ -11,14 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.ObjectStreamClass;
-import java.lang.management.GarbageCollectorMXBean;
-import java.lang.management.ManagementFactory;
 import java.time.Duration;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.*;
 
 @RestController
 @Slf4j
@@ -26,45 +21,22 @@ public class TestController {
 
     @GetMapping("/helloworld")
     public String helloworld(HttpServletRequest request, HttpServletResponse response) {
-        ResponseCookie cookie33 = ResponseCookie.from("bage", "yellowgoril").httpOnly(true).secure(true).path("/").
-                maxAge(Duration.ofHours(1))
-                .sameSite(SameSiteCookies.NONE.getValue())
-                .build();
-        // 设置Cookie Header
-        //response.setHeader(HttpHeaders.SET_COOKIE, cookie33.toString());
-        ResponseCookie cookie44 = ResponseCookie.from("bage444", "yellowgoril4444").httpOnly(true).secure(true).path("/").
-                maxAge(Duration.ofHours(1))
-                .sameSite(SameSiteCookies.NONE.getValue())
-                .build();
-
-        response.setHeader(HttpHeaders.SET_COOKIE, cookie44.toString());
-        response.addHeader(HttpHeaders.SET_COOKIE, cookie33.toString());
-
-//        Cookie cookie = new Cookie("wag2222", "yu2222");
-//        cookie.setSecure(true);
-//        cookie.setMaxAge(10 * 365 * 24 * 60 * 60);
-//        cookie.setHttpOnly(true);
-//        // 设置path到根目录
-//        cookie.setPath("/");
-//        response.addCookie(cookie);
-//        response.setHeader("set-cookie");
 
 
         return "beijign";
     }
 
-    public static void main(String[] args)
-            throws InterruptedException {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    public static void main(String[] args) {
+        System.out.println(RandomStringUtils.randomAlphanumeric(100));
 
-        numbers.stream().forEach(a->{
-            System.out.println("北极光");
-        });
-        numbers.parallelStream().forEach(a->{
+    }
 
-            System.out.println( Thread.currentThread().getName()+"===="+a);
-        });
-
+    @GetMapping("/wang")
+    public String wang(HttpServletRequest request, HttpServletResponse response) {
+        for (int i = 0; i < 1000; i++) {
+            String s = RandomStringUtils.randomAlphanumeric(1000);
+        }
+        return "beijign";
     }
 
 }
